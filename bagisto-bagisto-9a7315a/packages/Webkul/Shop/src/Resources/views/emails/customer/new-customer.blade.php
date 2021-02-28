b89d8afe8316a75f620ed54a3c70ac4c519f4414
bpmanzo <bpmanzo@student.apc.edu.ph>
2021-03-01 01:30:03 +0800

    Footer add email

--- bagisto-bagisto-9a7315a/packages/Webkul/Shop/src/Resources/views/emails/customer/new-customer.blade.php
+++ bagisto-bagisto-9a7315a/packages/Webkul/Shop/src/Resources/views/emails/customer/new-customer.blade.php
@@ -27,15 +27,14 @@
         </p>
     </div>
         <p>About Us:
-        <code>&lt;p&gt;
-        &lt;div&gt;
+        <p>
+        <div>
         We are Big Scoop Ice Cream - Pasay City Authorized Retailer! 
         We are happy to serve you one of the best ice cream brands in the 
         country. Big Scoop is served in various restaurants and hotels 
         nationwide! Order now and find happiness with every scoop!
-        &lt;/div&gt;
-        &lt;/p&gt;
-        </code> 
+        </div>
+        </p>
         </p>
 
 @endcomponent
